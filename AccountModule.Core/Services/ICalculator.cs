cbd0b96ed6b0f5cadbffefeb132110bd30580cd7
Keattisak Chinburarat <pommatt@gmail.com>
2021-07-28 16:38:21 +0700

    add new service

---
+++ AccountModule.Core/Services/ICalculator.cs
@@ -0,0 +1,29 @@
+﻿using Agoda.IoC.Core;
+
+namespace AccountModule.Core.Services
+{
+    public interface ICalculatorAdd
+    {
+        int Add(int first, int second);
+    }
+    public interface ICalculatorSub
+    {
+        int Sub(int first, int second);
+    }
+
+    [RegisterSingleton(For = typeof(ICalculatorAdd))]
+    [RegisterSingleton(For = typeof(ICalculatorSub))]
+    public class Calculator : ICalculatorAdd, ICalculatorSub
+    {
+        public int Add(int first, int second)
+        {
+            return first + second;
+        }
+
+        public int Sub(int first, int second)
+        {
+            return first - second;
+
+        }
+    }
+}
